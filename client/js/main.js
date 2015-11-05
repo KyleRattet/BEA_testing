8210e3a80199c37be534493fc027128edc6cf207
KyleRattet <krattet@gmail.com>
2015-11-05 16:43:37 -0700

    route testing

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,31 @@
 // add scripts
 
+
+
 $(document).on('ready', function() {
   console.log('sanity check!');
+// var id = process.env.ID;
+
+$.get(searchUrl, function( data ) {
+  $( ".result" ).html( data );
+  console.log(data);
 });
+ //  var payload =
+
+ // $.ajax({
+ //      url: '/info',
+ //      data: data
+ //    }).done(function(data){
+ //      console.log(data);
+ //    });
+
+$.get( "/info", function( data ) {
+  $( ".result" ).html( data );
+  console.log(data);
+});
+
+
+});
+
+
+
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var http = require("http");
+var request=require("request");
+
+
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//get data back
+router.get('/info', function(req, res, next) {
+  res.send('route testing');
+
+});
+
+
+
+
 
 module.exports = router;

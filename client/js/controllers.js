3b298dab7df2d68f72bba40bc363045b288d8f1e
KyleRattet <krattet@gmail.com>
2015-11-10 13:31:35 -0700

    button to get state data

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -11,21 +11,35 @@ app.controller("MainController", ['$scope', '$http', 'httpFactory', function($sc
  //    });
  //  };
 
- var parameters = {
-   state: 01,
-   description: 'test'
-};
+
+  //use this to build out query
 
 
   getInfo = function (url) {
+
+    //use to build out query
+    var parameters = {
+       state: $scope.state_select,
+       description: 'test'
+        };
     httpFactory.get(url, {params: parameters})
     .then(function(response){
         console.log(response, "api info response");
     });
   };
 
-  // getInfo('/api/v1/data/bea');
+ $scope.getData = function () {
+  console.log($scope.state_select, "state")
   getInfo('/api/v1/data/census');
+ };
+
+
+
+
+
+
+  // getInfo('/api/v1/data/bea');
+
 
   // getBEADATA = function () {
   //    var info = $http.get("http://www.bea.gov/api/data/?&UserID="+BEA_id+"&method=GetData&datasetname=RegionalData&KeyCode=PCDPI_SI&GeoFIPS=STATE&Year=2012&ResultFormat=JSON&%27")
